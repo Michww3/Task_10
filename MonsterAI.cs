967a15086603850c613ffa86148c69fc60892524
Michww3 <michww3@gmail.com>
2024-06-27 18:12:12 +0300

    new

---
+++ MonsterAI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+abstract class MonsterAI
+{
+    public void TemplateMethod()
+    {
+        Attack();
+        Defence();
+        Heal();
+        //Hook1();
+    }
+
+    protected void Attack()
+    {
+        Console.WriteLine("Attack");
+    }
+    protected void Defence()
+    {
+        Console.WriteLine("Defence");
+
+    }
+    protected abstract void Heal();
+
+}
